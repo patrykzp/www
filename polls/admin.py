66b3fb98ddc802895b8580e8c3ee2213cf2f9e09
patrykz <patryk.zajkowski@infotech.edu.pl>
2022-09-19 14:37:45 +0200

    pierwszy commi5

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from . models import Question, Answer, Choice
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    date_hierarchy = 'pub_date'
+    list_display = ('question_text', 'pub_date', 'type')
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('user_answer', 'question')
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('choice_text', 'question','votes')
 
 # Register your models here.
